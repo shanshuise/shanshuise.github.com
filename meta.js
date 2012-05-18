9c7cc4666b3ccf9e744b15187b2be7436c2b86e0
shan <shanshuise@gmail.com>
2012-05-19 03:37:31 +0800

    nnnnn

--- meta.js
+++ meta.js
@@ -1,24 +1,12 @@
 ﻿{
-	"site_name": "黑猫日记",
-	"copyright": "黑猫日记 2012",
+	"site_name": "sweepram",
+	"copyright": "sweepram 2012",
 	"pages": [
-		{"title": "weibo", "link": "http://weibo.com/yqjun123"},
+		{"title": "weibo", "link": "http://weibo.com/xxxxx"},
 		{"title": "demo", "link": "demo"},
 		{"title": "about", "link": "about"}
 	],
 	"articles": [
-		{"title": "chrome扩展-微博桌面提醒", "file": "2012-05-06-chrome-weibo-notice"},
-		{"title": "初探Backbone & new blog()", "file": "2012-04-26-new-blog-program"},
-		{"title": "腾讯网页重构面试之点滴", "file": "2012-04-23-tencent-interview"},
-		{"title": "井字棋游戏的实现", "file": "2012-04-18-tic-tac-toe"},
-		{"title": "七巧板游戏的实现", "file": "2012-04-14-tangram-demo"},
-		{"title": "简易AboutMe (简历)", "file": "2012-04-13-aboutme"},
-		{"title": "sh脚本之懒人git", "file": "2012-04-11-sh-git"},
-		{"title": "HTML5本地留言板", "file": "2012-04-06-h5-msg-board"},
-		{"title": "About This Blog", "file": "2012-03-29-about-this-blog"},
-		{"title": "低仿Path菜单", "file": "2012-03-22-copy-path-menu-incompletely"},
-		{"title": "在线前端测试工具", "file": "2012-03-20-f2e-online-debugging-tool"},
-		{"title": "关于前路", "file": "2012-03-14-about-my-way"},
-		{"title": "Hello World", "file": "2012-03-10-hello-world"}
+			{"title": "一个周末的午夜", "file": "2012-05-29"}
 	]
 }
